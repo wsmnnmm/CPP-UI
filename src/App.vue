70c704a1433fe8bf6315365c55703a3153ad33d3
wsmnnmm <1832004940@qq.com>
2022-03-30 15:43:03 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Wsm />
+</template>
+
+<script>
+import Wsm from "./components/wsm.vue";
+
+export default {
+  name: "App",
+  components: {
+    Wsm,
+  },
+};
+</script>
---
+++ src/components/wsm.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>这是一个组件</div>
+</template>
