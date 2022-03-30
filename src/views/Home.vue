013d28a6df7b20e03d8e5f797f9dc1701a9ab3a9
wsmnnmm <1832004940@qq.com>
2022-03-30 21:19:11 +0800

    初始化aside的html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>cppUI</h1>
       <h2>一个厉害的UI框架</h2>
       <p class="actions">
-        <a>GitHub</a>
-        <a>开始</a>
+        <a href="https://github.com">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
