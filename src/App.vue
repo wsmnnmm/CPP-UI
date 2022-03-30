9349c0da0ebc9ef9086dcf9468ec9fbf348f1284
wsmnnmm <1832004940@qq.com>
2022-03-30 20:09:23 +0800

    封装Topnav组件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <div>hi</div>
-  导航栏 | <router-link to="/">Home</router-link> |
-  <router-link to="/doc">Doc</router-link>
-  <hr />
   <router-view />
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <div class="topnav">
-      <div class="logo">LOGO</div>
-      <div class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </div>
-    </div>
+    <Topnav />
     <div class="banner">
       <h1>cppUI</h1>
       <h2>一个厉害的UI框架</h2>
@@ -17,22 +11,10 @@
     </div>
   </div>
 </template>
-<style lang="scss" scoped>
-.topnav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
-    }
-  }
-}
-</style>
+<script lang="ts">
+import Topnav from "../components/Topnav.vue";
+export default {
+  components: { Topnav },
+};
+</script>
+<style lang="scss" scope></style>
