e76153f496c5e0053c75e415ad0bbc4b2424f46a
wsmnnmm <1832004940@qq.com>
2022-04-03 21:44:03 +0800

    switch CSS 2

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -24,7 +24,7 @@ button {
   height: $h;
   width: $h * 2;
   border: none;
-  background: grey;
+  background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
 }
@@ -36,15 +36,26 @@ span {
   height: $h2;
   width: $h2;
   border-radius: $h/2;
-  transition: left 250ms;
+  transition: all 250ms;
 }
 button.checked {
-  background: blue;
+  background: #1890ff;
 }
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
 }
 button:focus {
   outline: none;
+}
+button:active {
+  > span {
+    width: $h2 + 4px;
+  }
+}
+button.checked:active {
+  > span {
+    width: $h2 + 4px;
+    margin-left: -4px;
+  }
 }
 </style>
