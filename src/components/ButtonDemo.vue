4ee25d76efbcb4af0e89739dda1250cba91027d0
wsmnnmm <1832004940@qq.com>
2022-05-22 11:12:48 +0800

    添加Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,3 +1,11 @@
 <template>
   <div>Button 示例</div>
+  <h1>示例展示</h1>
+  <Button>你好呀</Button>
 </template>
+<script lang = "ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: { Button },
+};
+</script>
