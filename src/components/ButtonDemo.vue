86b5b2294024dc924a562820a42ba3638b84b700
wsmnnmm <1832004940@qq.com>
2022-05-22 17:26:28 +0800

    button基础样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,10 +2,19 @@
   <div>Button 示例</div>
   <h1>示例展示</h1>
   <Button>你好呀</Button>
+  <Button theme="button">你好呀</Button>
+  <Button theme="link">你好呀</Button>
+  <Button theme="text">你好呀</Button>
 </template>
 <script lang = "ts">
 import Button from "../lib/Button.vue";
 export default {
   components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log("hi");
+    };
+    return { onClick };
+  },
 };
 </script>
