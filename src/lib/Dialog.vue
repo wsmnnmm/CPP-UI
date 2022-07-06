e6a46e6b8ad56faaf178989fab7ba65ab33e2518
wsmnnmm <1832004940@qq.com>
2022-07-06 13:35:18 +0800

    创建Dialog组件

---
+++ src/lib/Dialog.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="cpp-dialog-overlay">遮障</div>
+  <div class="cpp-dialog-wrapper">
+    <header>标题</header>
+    <main>
+      <p>第一行字</p>
+      <p>第二行字</p>
+    </main>
+    <footer>
+      <Button>ok</Button>
+      <Button>Cancel</Button>
+    </footer>
+  </div>
+</template>
+<script>
+import Button from "../lib/Button.vue";
+export default {
+  components: { Button },
+};
+</script>
