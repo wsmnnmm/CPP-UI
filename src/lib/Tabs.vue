204295548fc014bcf2e8f70409c7f0d2b131c1ef
wsmnnmm <1832004940@qq.com>
2022-07-18 11:17:23 +0800

    Tabs css 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
-    <component v-for="(c, index) in defaults" :is="c" :key="index" />
+  <div class="cpp-tabs">
+    <div class="cpp-tabs-nav">
+      <div class="cpp-tabs-nav-item" v-for="(t, index) in titles" :key="index">
+        {{ t }}
+      </div>
+    </div>
+    <div class="cpp-tabs-content">
+      <component v-for="(c, index) in defaults" :is="c" :key="index" />
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -24,3 +30,29 @@ export default {
   },
 };
 </script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.cpp-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
