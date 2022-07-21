ee66c718553d2fc818345918a512214357a2cd3b
wsmnnmm <1832004940@qq.com>
2022-07-21 15:47:44 +0800

    features 样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -6,8 +6,7 @@
       </svg>
     </div>
     <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
+      <li><router-link to="/doc">文档</router-link></li>
     </ul>
     <span class="toggleAside" @click="toggleMenu"></span>
   </div>
