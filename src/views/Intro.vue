456be9441ce7e6d6d9483c64bdcb80b9aec5690c
wsmnnmm <1832004940@qq.com>
2022-07-25 16:31:29 +0800

    填充文档内容

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,3 +1,11 @@
 <template>
-  <div>intro</div>
+  <article>
+    <h1>介绍</h1>
+    <p>cpp UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
+    <p>
+      这款组件库使用了最新的技术栈，全程手写，组件库源码不采用任何第三方库。源码拥有良好的编码风格，每个组件的代码都足够简洁，十分易读。
+    </p>
+    <p>文档完整，拥有完整的组件使用示例代码。</p>
+    下一节：<a href="#/doc/install">安装</a>
+  </article>
 </template>
