6c137ec841b324f671b2e56ff9af9fa5f40aa44c
wsmnnmm <1832004940@qq.com>
2022-07-26 10:52:14 +0800

    支持markdown 文件

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,15 +1,13 @@
 <template>
-  <article class="markdown-body">
-    <h1>介绍</h1>
-    <p>CPP UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
-    <p>
-      这款组件库使用了最新的技术栈，全程手写，组件库源码不采用任何第三方库。源码拥有良好的编码风格，每个组件的代码都足够简洁，十分易读。
-    </p>
-    <p>文档完整，拥有完整的组件使用示例代码。</p>
-    <p>
-      如果你觉得对官网和UI组件源码有兴趣，那么可以访问我的
-      <a href="https://github.com/wsmnnmm/CPP-UI" target="_blank">GitHub</a>
-    </p>
-    查看源码 下一节：<a href="#/doc/install">安装</a>
-  </article>
+  <article class="markdown-body" v-html="md"></article>
 </template>
+<script >
+import md from "../markdown/intro.md";
+export default {
+  data() {
+    return {
+      md,
+    };
+  },
+};
+</script>
