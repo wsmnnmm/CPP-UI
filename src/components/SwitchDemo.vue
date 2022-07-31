efc86cc50fcef6032ad43653a09009b498491d95
wsmnnmm <1832004940@qq.com>
2022-07-31 08:40:47 +0800

    展示源代码并且高亮

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,25 +4,43 @@
     <div class="demo">
       <h2>常规用法</h2>
       <div class="demo-component">
-        <component :is="Switch1Demo"></component>
+        <component :is="Switch1Demo" />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{ Switch1Demo.__sourceCode }}</pre>
+        <pre
+          class="language-html"
+          v-html="
+            Prism.highlight(
+              Switch1Demo.__sourceCode,
+              Prism.languages.html,
+              'html'
+            )
+          "
+        />
       </div>
     </div>
     <div class="demo">
       <h2>支持 disabled</h2>
       <div class="demo-component">
-        <component :is="Switch2Demo"></component>
+        <component :is="Switch2Demo" />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{ Switch2Demo.__sourceCode }}</pre>
+        <pre
+          class="language-html"
+          v-html="
+            Prism.highlight(
+              Switch2Demo.__sourceCode,
+              Prism.languages.html,
+              'html'
+            )
+          "
+        />
       </div>
     </div>
   </div>
@@ -33,11 +51,12 @@ import Switch from "../lib/Switch.vue";
 import Button from "../lib/Button.vue";
 import Switch1Demo from "./Switch1.demo.vue";
 import Switch2Demo from "./Switch2.demo.vue";
-console.log(Switch1Demo.__sourceCode);
+import "prismjs";
+import "prismjs/themes/prism.css";
+const Prism = (window as any).Prism;
 import { ref } from "vue";
 export default {
   components: {
-    Switch,
     Button,
   },
   setup() {
@@ -46,6 +65,7 @@ export default {
       bool,
       Switch1Demo,
       Switch2Demo,
+      Prism,
     };
   },
 };
